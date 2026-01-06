2172587059bd516f7b5763c1c5c0ec0d2fbcfe10
Aymane <motadaymane@gmail.com>
2026-01-06 19:48:25 +0100

    s

--- app/[locale]/can-2025/page.tsx
+++ app/[locale]/can-2025/page.tsx
@@ -276,7 +276,7 @@ export default function Can2025Page() {
         { date: '24 décembre', time: '16h00', team1: 'Algérie', team2: 'Soudan', group: 'Groupe E', type: 'Phase de groupes', score: '3 - 0' },
         { date: '28 décembre', time: '18h30', team1: 'Algérie', team2: 'Burkina Faso', group: 'Groupe E', type: 'Phase de groupes', score: '1 - 0' },
         { date: '31 décembre', time: '17h00', team1: 'Guinée équatoriale', team2: 'Algérie', group: 'Groupe E', type: 'Phase de groupes', score: '1 - 3' },
-        { date: '6 janvier', time: '17h00', team1: 'Algérie', team2: 'RD Congo', group: '8es de finale', type: 'Élimination directe', score: '0 - 0' },
+        { date: '6 janvier', time: '17h00', team1: 'Algérie', team2: 'RD Congo', group: '8es de finale', type: 'Élimination directe', score: '0 - 0 (1-0)' },
       ]
     },
     {
@@ -331,7 +331,7 @@ export default function Can2025Page() {
         { date: '28 décembre', time: '21h00', team1: 'Côte d\'Ivoire', team2: 'Cameroun', group: 'Groupe F', type: 'Phase de groupes', score: '1 - 1' },
         { date: '29 décembre', time: '17h00', team1: 'Zimbabwe', team2: 'Afrique du Sud', group: 'Groupe B', type: 'Phase de groupes', score: '2 - 3' },
         { date: '31 décembre', time: '20h00', team1: 'Gabon', team2: 'Côte d\'Ivoire', group: 'Groupe F', type: 'Phase de groupes', score: '2 - 3' },
-        { date: '6 janvier', time: '20h00', team1: '1er F', team2: '2e E', group: '8es de finale', type: 'Élimination directe' },
+        { date: '6 janvier', time: '20h00', team1: 'Côte d\'Ivoire', team2: 'Burkina Faso', group: '8es de finale', type: 'Élimination directe' },
         { date: '10 janvier', time: '17h00', team1: 'TBD', team2: 'TBD', group: 'Quart de finale', type: 'Élimination directe' },
       ]
     },
