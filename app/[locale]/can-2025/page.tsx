eb418f8e716b2562de88478b2ee572543b8f1b3f
Aymane <motadaymane@gmail.com>
2026-01-09 22:05:59 +0100

    sds

--- app/[locale]/can-2025/page.tsx
+++ app/[locale]/can-2025/page.tsx
@@ -251,7 +251,7 @@ export default function Can2025Page() {
         { date: '26 décembre', time: '21h00', team1: 'Maroc', team2: 'Mali', group: 'Groupe A', type: 'Phase de groupes', score: '1 - 1' },
         { date: '29 décembre', time: '20h00', team1: 'Zambie', team2: 'Maroc', group: 'Groupe A', type: 'Phase de groupes', score: '0 - 3' },
         { date: '4 janvier', time: '17h00', team1: 'Maroc', team2: 'Tanzanie', group: '8es de finale', type: 'Élimination directe', score: '1 - 0' },
-        { date: '9 janvier', time: '20h00', team1: 'TBD', team2: 'TBD', group: 'Quart de finale', type: 'Élimination directe' },
+        { date: '9 janvier', time: '20h00', team1: 'Maroc', team2: 'Cameroun', group: 'Quart de finale', type: 'Élimination directe', score: '2 - 0' },
         { date: '14 janvier', time: '21h00', team1: 'TBD', team2: 'TBD', group: '1/2 finale', type: 'Élimination directe' },
         { date: '18 janvier', time: '20h00', team1: 'TBD', team2: 'TBD', group: 'Finale', type: 'Finale' },
       ]
