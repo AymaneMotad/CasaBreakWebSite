43e9e96dc59290bfcd4dd9105cd763046fbaf71f
Aymane <motadaymane@gmail.com>
2026-01-10 19:22:37 +0100

    h

--- app/[locale]/can-2025/page.tsx
+++ app/[locale]/can-2025/page.tsx
@@ -304,7 +304,7 @@ export default function Can2025Page() {
         { date: '29 décembre', time: '17h00', team1: 'Angola', team2: 'Égypte', group: 'Groupe B', type: 'Phase de groupes', score: '0 - 0' },
         { date: '31 décembre', time: '20h00', team1: 'Mozambique', team2: 'Cameroun', group: 'Groupe F', type: 'Phase de groupes', score: '1 - 2' },
         { date: '5 janvier', time: '17h00', team1: 'Égypte', team2: 'Bénin', group: '8es de finale', type: 'Élimination directe', score: '3 - 1' },
-        { date: '10 janvier', time: '20h00', team1: 'TBD', team2: 'TBD', group: 'Quart de finale', type: 'Élimination directe' },
+        { date: '10 janvier', time: '20h00', team1: 'Nigeria', team2: 'Algérie', group: 'Quart de finale', type: 'Élimination directe', score: '2 - 0' },
       ]
     },
     {
